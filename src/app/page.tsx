b4be9b9d5c1d1acfc958351714e10a3c5e9438fd
Kaveenxz <kaveenhansithx@gmail.com>
2024-08-08 09:46:58 +0530

    Updated Footer component to use Next Image and removed Lucide React icons; refactored page structure in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,31 +10,33 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="mx-20 my-20">
-      <div>
-        <HeroSection />
+    <>
+      <div className="mx-20 my-20">
+        <div>
+          <HeroSection />
+        </div>
+        <div className="mt-20">
+          <AboutUs />
+        </div>
+        <div className="mt-20">
+          <Products />
+        </div>
+        <div className="mt-20">
+          <Activities />
+        </div>
+        <div className="mt-20">
+          <UpCommingEvents />
+        </div>
+        <div className="mt-20">
+          <Blog />
+        </div>
+        <div className="mt-20">
+          <ContactUs />
+        </div>
+
       </div>
       <div className="mt-20">
-        <AboutUs />
-      </div>
-      <div className="mt-20">
-        <Products/>
-      </div>
-      <div className="mt-20">
-        <Activities/>
-      </div>
-      <div className="mt-20">
-        <UpCommingEvents/>
-      </div>
-      <div className="mt-20">
-        <Blog/>
-      </div>
-      <div className="mt-20">
-        <ContactUs/>
-      </div>
-      <div className="mt-20">
-        <Footer/>
-      </div>
-    </div>
+        <Footer />
+      </div></>
   );
 }
