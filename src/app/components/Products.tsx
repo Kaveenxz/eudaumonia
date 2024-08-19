b6727ba3a8f60eb600e153603dd2ea97181a7102
Kaveenxz <kaveenhansithx@gmail.com>
2024-08-19 14:14:46 +0530

    Updated styling and layout of Products component

--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -31,20 +31,20 @@ function Products() {
     return (
         <div>
             <div>
-                <h1 className='text-5xl font-semibold text-[#D31145] text-center'>OUR PRODUCTS</h1>
-                <p className='text-lg text-center mt-8'>Find The Right Insurance For You And Your Loved Ones</p>
+                <h1 className='text-5xl font-semibold text-[#D31145] text-center max-md:text-3xl'>OUR PRODUCTS</h1>
+                <p className='text-lg text-center mt-2 max-md:text-sm'>Find The Right Insurance For You And Your Loved Ones</p>
             </div>
 
-            <div className='grid grid-cols-2 mt-8 gap-8'>
+            <div className='grid grid-cols-1 md:grid-cols-2 mt-6 gap-8'>
                
 
                 {
                     products.map((product:any) => (
                         <div className='border-2 border-gray-300 rounded-2xl flex py-5 px-8 gap-5 cursor-pointer' key={product.id} onClick={()=> handleClick(product.id)}>
-                        <div className='w-4 h-4 rounded-full bg-blue-400'></div>
+                        <div className='w-6 h-6 rounded-full bg-blue-400'></div>
                         <div>
-                            <h3 className='text-xl font-semibold'>{product.title}</h3>
-                            <p>{product.paragraph}</p>
+                            <h3 className='text-xl font-semibold max-md:text-lg'>{product.title}</h3>
+                            <p className='max-md:text-sm'>{product.paragraph}</p>
                         </div>
                     </div>
                     ))
