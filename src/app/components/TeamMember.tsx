df766d91a0d914ff26e825ed1075bf762110a570
Kaveenxz <kaveenhansithx@gmail.com>
2024-10-02 13:00:25 +0530

    Refactored TeamMember component and MeetOurTeam page to use react-query for data fetching and removed hardcoded team member data

--- src/app/components/TeamMember.tsx
+++ src/app/components/TeamMember.tsx
@@ -1,22 +1,31 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-export default function TeamMember({ name, title, description, image, socials, id }:any) {
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import avt1 from '@/app/images/Avatar-1.png'
+
+export default function TeamMember({ id, name, title, imageUrl, githubLink, twitterLink, linkedinLink }:any) {
         const route = useRouter()
-        const handleClick = (id:any) => {
-            route.push('/team-member/'+id)
+        const handleClick = (key:any) => {
+            route.push('/team-member/'+key)            
         }
+        
     return (
       <div className="bg-[#F9FAFB] p-6 rounded-lg text-center" onClick={()=> {handleClick(id)}}>
-        <Image src={image} alt={name} className="w-24 h-24 mx-auto rounded-full object-cover mb-4" width={96} height={96}/>
+        <Image src={imageUrl || avt1} alt={name} className="w-24 h-24 mx-auto rounded-full object-cover mb-4" width={96} height={96}/>
         <h3 className="text-lg font-bold">{name}</h3>
-        <p className="text-sm text-gray-600">{title}</p>
-        <p className="text-gray-500 mt-2">{description}</p>
+        <p className="text-gray-500 mt-2">{title}</p>
         <div className="mt-4 flex justify-center space-x-3">
-          {socials.map(({ icon, link }:any, index:any) => (
-            <a href={link} key={index} className="text-gray-600 hover:text-gray-800">
-              {icon}
-            </a>
-          ))}
+            
+              <a href={githubLink} key={githubLink} className="text-gray-600 hover:text-gray-800">
+                <FaGithub/>
+              </a>
+              <a href={twitterLink} key={twitterLink} className="text-gray-600 hover:text-gray-800">
+                <FaTwitter/>
+              </a>
+              <a href={linkedinLink} key={linkedinLink} className="text-gray-600 hover:text-gray-800">
+                <FaLinkedin/>
+              </a>
+            
         </div>
       </div>
     );
--- src/app/team-member/page.tsx
+++ src/app/team-member/page.tsx
@@ -8,123 +8,142 @@ import avt5 from '@/app/images/Avatar-5.png'
 import avt6 from '@/app/images/Avatar-6.png'
 import avt7 from '@/app/images/Avatar-7.png'
 import avt8 from '@/app/images/Avatar.png'
-import { Twitter } from "lucide";
 import { useRouter } from "next/navigation";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { useQuery } from "react-query";
+import { getallTeamMembers } from "../api/team-member/api";
 
-const teamMembers = [
-  {
-    id:1,
-    name: "Olivia Rhye",
-    title: "Co-founder of Opendoor",
-    description: "Early staff at Spotify and Clearbit.",
-    image: avt1, // Replace with actual images or links
-    socials: [
-      { icon: <FaTwitter />, link: "#" },
-      { icon: <FaLinkedin />, link: "#" },
-      { icon: <FaGithub />, link: "#" },
-    ],
-  },
-  {
-    id:2,
-    name: "Phoenix Baker",
-    title: "Lead engineering teams",
-    description: "Worked at Figma, Pitch, and Protocol Labs.",
-    image: avt2,
-    socials: [
-      { icon: <FaTwitter />, link: "#" },
-      { icon: <FaLinkedin />, link: "#" },
-      { icon: <FaGithub />, link: "#" },
-    ],
-  },
-  {
-    id:3,
-    name: "Olivia Rhye",
-    title: "Co-founder of Opendoor",
-    description: "Early staff at Spotify and Clearbit.",
-    image: avt8, // Replace with actual images or links
-    socials: [
-      { icon: <FaTwitter />, link: "#" },
-      { icon: <FaLinkedin />, link: "#" },
-      { icon: <FaGithub />, link: "#" },
-    ],
-  },
-  {
-    id:4,
-    name: "Phoenix Baker",
-    title: "Lead engineering teams",
-    description: "Worked at Figma, Pitch, and Protocol Labs.",
-    image: avt3,
-    socials: [
-      { icon: <FaTwitter />, link: "#" },
-      { icon: <FaLinkedin />, link: "#" },
-      { icon: <FaGithub />, link: "#" },
-    ],
-  },
-  {
-    id:5,
-    name: "Olivia Rhye",
-    title: "Co-founder of Opendoor",
-    description: "Early staff at Spotify and Clearbit.",
-    image: avt4, // Replace with actual images or links
-    socials: [
-      { icon: <FaTwitter />, link: "#" },
-      { icon: <FaLinkedin />, link: "#" },
-      { icon: <FaGithub />, link: "#" },
-    ],
-  },
-  {
-    id:6,
-    name: "Phoenix Baker",
-    title: "Lead engineering teams",
-    description: "Worked at Figma, Pitch, and Protocol Labs.",
-    image: avt5,
-    socials: [
-      { icon: <FaTwitter />, link: "#" },
-      { icon: <FaLinkedin />, link: "#" },
-      { icon: <FaGithub />, link: "#" },
-    ],
-  },
-  {
-    id:7,
-    name: "Olivia Rhye",
-    title: "Co-founder of Opendoor",
-    description: "Early staff at Spotify and Clearbit.",
-    image: avt6, // Replace with actual images or links
-    socials: [
-      { icon: <FaTwitter />, link: "#" },
-      { icon: <FaLinkedin />, link: "#" },
-      { icon: <FaGithub />, link: "#" },
-    ],
-  },
-  {
-    id:8,
-    name: "Phoenix Baker",
-    title: "Lead engineering teams",
-    description: "Worked at Figma, Pitch, and Protocol Labs.",
-    image: avt7,
-    socials: [
-      { icon: <FaTwitter />, link: "#" },
-      { icon: <FaLinkedin />, link: "#" },
-      { icon: <FaGithub />, link: "#" },
-    ],
-  },
-  
-];
+// const teamMembers = [
+//   {
+//     id:1,
+//     name: "Olivia Rhye",
+//     title: "Co-founder of Opendoor",
+//     description: "Early staff at Spotify and Clearbit.",
+//     image: avt1, // Replace with actual images or links
+//     socials: [
+//       { icon: <FaTwitter />, link: "#" },
+//       { icon: <FaLinkedin />, link: "#" },
+//       { icon: <FaGithub />, link: "#" },
+//     ],
+//   },
+//   {
+//     id:2,
+//     name: "Phoenix Baker",
+//     title: "Lead engineering teams",
+//     description: "Worked at Figma, Pitch, and Protocol Labs.",
+//     image: avt2,
+//     socials: [
+//       { icon: <FaTwitter />, link: "#" },
+//       { icon: <FaLinkedin />, link: "#" },
+//       { icon: <FaGithub />, link: "#" },
+//     ],
+//   },
+//   {
+//     id:3,
+//     name: "Olivia Rhye",
+//     title: "Co-founder of Opendoor",
+//     description: "Early staff at Spotify and Clearbit.",
+//     image: avt8, // Replace with actual images or links
+//     socials: [
+//       { icon: <FaTwitter />, link: "#" },
+//       { icon: <FaLinkedin />, link: "#" },
+//       { icon: <FaGithub />, link: "#" },
+//     ],
+//   },
+//   {
+//     id:4,
+//     name: "Phoenix Baker",
+//     title: "Lead engineering teams",
+//     description: "Worked at Figma, Pitch, and Protocol Labs.",
+//     image: avt3,
+//     socials: [
+//       { icon: <FaTwitter />, link: "#" },
+//       { icon: <FaLinkedin />, link: "#" },
+//       { icon: <FaGithub />, link: "#" },
+//     ],
+//   },
+//   {
+//     id:5,
+//     name: "Olivia Rhye",
+//     title: "Co-founder of Opendoor",
+//     description: "Early staff at Spotify and Clearbit.",
+//     image: avt4, // Replace with actual images or links
+//     socials: [
+//       { icon: <FaTwitter />, link: "#" },
+//       { icon: <FaLinkedin />, link: "#" },
+//       { icon: <FaGithub />, link: "#" },
+//     ],
+//   },
+//   {
+//     id:6,
+//     name: "Phoenix Baker",
+//     title: "Lead engineering teams",
+//     description: "Worked at Figma, Pitch, and Protocol Labs.",
+//     image: avt5,
+//     socials: [
+//       { icon: <FaTwitter />, link: "#" },
+//       { icon: <FaLinkedin />, link: "#" },
+//       { icon: <FaGithub />, link: "#" },
+//     ],
+//   },
+//   {
+//     id:7,
+//     name: "Olivia Rhye",
+//     title: "Co-founder of Opendoor",
+//     description: "Early staff at Spotify and Clearbit.",
+//     image: avt6, // Replace with actual images or links
+//     socials: [
+//       { icon: <FaTwitter />, link: "#" },
+//       { icon: <FaLinkedin />, link: "#" },
+//       { icon: <FaGithub />, link: "#" },
+//     ],
+//   },
+//   {
+//     id:8,
+//     name: "Phoenix Baker",
+//     title: "Lead engineering teams",
+//     description: "Worked at Figma, Pitch, and Protocol Labs.",
+//     image: avt7,
+//     socials: [
+//       { icon: <FaTwitter />, link: "#" },
+//       { icon: <FaLinkedin />, link: "#" },
+//       { icon: <FaGithub />, link: "#" },
+//     ],
+//   },
+
+// ];
 export default function MeetOurTeam() {
-    const route = useRouter()
-    const handleClick = (id:any) => {
-        route.push('/team-member/'+id)
-    }
+  const route = useRouter()
+
+  const { data: teamMembers, isLoading, isError, error } = useQuery('teamMembers', getallTeamMembers);
+  console.log(teamMembers)
+
+  if (isLoading) {
+    return <div>Loading...</div>; 
+  }
+
+  if (isError) {
+    route.push('not-found')
+  }
+
+  const handleClick = (id: any) => {
+    route.push('/team-member/' + id)
+  }
 
   return (
-    <section className="py-12 mx-4 md:mx-10 lg:mx-20 my-10 lg:my-20">
-      <div className="max-w-7xl mx-auto px-6 lg:px-8">
-        <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">Meet our team</h2>
-        <p className="text-center text-gray-600 mb-12">Our philosophy is simple — hire a team of diverse, passionate people and foster a culture that empowers you to do your best work.</p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 cursor-pointer">
-          {teamMembers.map((member, index) => (
-            <TeamMember key={index} {...member}/>
+    <section id="meetourteam" className="bg-gray-100 py-16">
+      <div className="container mx-auto text-center">
+        <h2 className="text-4xl font-bold mb-10">Meet Our Team</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {teamMembers?.map((member: any) => (
+            <TeamMember
+              id={member.id}
+              name={member.name}
+              title={member.description}
+              imageUrl={member.imagePath}
+              githubLink={member.link2}
+              linkedinLink={member.link1}
+              twitterLink={member.link3}
+            />
           ))}
         </div>
       </div>
